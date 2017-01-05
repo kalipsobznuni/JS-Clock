6c9350c65c218036c13e67406e0e338dbdfcdf08
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-01-05 07:35:36 +0400

    min and hour hands fixed

--- js.js
+++ js.js
@@ -1,12 +1,12 @@
 const secondHand = document.querySelector('.second-hand');
-const minsdHand = document.querySelector('.min-hand');
-const hourdHand = document.querySelector('.hour-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 function setDate(){
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const secondsDegrees = ((seconds/60)*360)+90;
+  secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
@@ -15,9 +15,8 @@ function setDate(){
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
 
-
-};
   setInterval(setDate,1000);
 
 setDate();
